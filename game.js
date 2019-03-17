38828d5ebd3516abcd5b9efd436c47b01b931398
orestwojtowicz <damianwojtowicz94@gmail.com>
2019-03-17 15:38:05 +0100

    main logic of the game has been added

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 // empty array for holding user clicked colour
 var userClickedPattern = [];
 
@@ -11,80 +10,141 @@ var gamePattern = [];
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+// level variable
 
-// random number generator
+var level = 0;
 
-   $(".btn").click(function(){ 
-   var clickedButtonAtrr = $(this).attr("id");
+var flag = false;
+
+// random number generator click
+
+$(".btn").click(function () {
+    var clickedButtonAtrr = $(this).attr("id");
     userClickedPattern.push(clickedButtonAtrr);
     playSound(clickedButtonAtrr);
-    animatePress(clickedButtonAtrr);   
+    animatePress(clickedButtonAtrr);
     
+  checkAnswer(userClickedPattern.length-1);
 });
 
 
 
+// key detections
+
+
+$(document).keypress(function () {
+
+
+
+    if (!flag) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        flag = true;
+
+    }
+  
+
+
+
+
+});
+
+function nextSequence() {
+  userClickedPattern = [];
     
-    function nextSequence() {
- 
+    level++;
+    
+    $("#level-title").text("Level " + level);
+    
+    var randomNumber = Math.floor((Math.random() * 4));
 
-        
-    var randomNumber = Math.floor((Math.random()*4));
-        
     var randomChosenColour = buttonColours[randomNumber];
-        
-    gamePattern.push(randomChosenColour);
-        
-    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-        playSound(randomChosenColour);
+    gamePattern.push(randomChosenColour);
+    
+   
+    
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+
+
+
+
+
+}
+
+
+// wywolac to po tym jak uzytkownik kliknie w odpowiedz
+// podajac index of the last answer in the user's sequence
+
+function checkAnswer(currentLevel) {
+    //var userClickedPattern = []; user answers
+    //var gamePattern = []; random values from computer
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      //.game-over
+            
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+             $("body").removeClass("game-over");
+        },100);
         
-    }    
+        $("#level-title").text("Game Over, Press any Key to Play Again");
+        
+        startOver();
+        
+    }  
+    
+    
+}
+
+function startOver() {
+    level = 0;
+    flag = false;
+    gamePattern = [];
+}
 
 
 
 function playSound(name) {
-    
- var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
+
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
 
-function animatePress(currentColour){
+function animatePress(currentColour) {
     //css pressed class
-    
+
     var time = 100;
-    
-    $("#"+currentColour).addClass("pressed");
-    setTimeout(function(){
-        $("#"+currentColour).removeClass("pressed");
-    
+
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+
     }, time);
-    
-    
+
+
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// to do:
+// when key is pressed call nextSequence()
+// tylko wywolac za pierwszym razem
+// po nacisnieciu przycisku zmienic h1 do Level 0
+// kiedy nextSequence jest wywolana zwiekszysc level
