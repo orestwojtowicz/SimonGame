39f6081842f73c8ff2179ec6b810bc7380d4573f
orestwojtowicz <damianwojtowicz94@gmail.com>
2019-03-17 13:35:50 +0100

    nextSequence(), click(), mp3

--- game.js
+++ game.js
@@ -1,4 +1,8 @@
 
+// empty array for holding user clicked colour
+var userClickedPattern = [];
+
+
 // empty array for holding random colour
 
 var gamePattern = [];
@@ -8,15 +12,67 @@ var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 
-function nextSequence() {
-    
-    var randomNumber = Math.floor((Math.random()*4));
+// random number generator
 
+   $(".btn").click(function(){ 
+   var clickedButtonAtrr = $(this).attr("id");
+    userClickedPattern.push(clickedButtonAtrr);
+    playSound(clickedButtonAtrr);
+       
+    
+});
+
+
+
+    
+    function nextSequence() {
+ 
+
+        
+    var randomNumber = Math.floor((Math.random()*4));
+        
     var randomChosenColour = buttonColours[randomNumber];
-    
-    $("#"+randomChosenColour).css("background-color", buttonColours);
-    
+        
     gamePattern.push(randomChosenColour);
+        
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+        playSound(randomChosenColour);
+        
+    }    
+
+
+
+function playSound(name) {
+    
+ var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
